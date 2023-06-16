8b5da643e199012e26a987ceb4046b125d43a013
TzuHanLiang <crocodilestear@gmail.com>
2023-06-16 15:33:13 +0800

    feat: use aave usdt

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,36 +2,46 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract Vault is ERC4626 {
     // Info: create your variables and immutables (20230615 - tzuhan)
-    ERC20 private immutable _vUSDT;
+    // ERC20 private immutable _vUSDT;
+
+    IERC20 public usdt;
 
     // Info: a mapping that checks if a user has deposited (20230615 - tzuhan)
     mapping(address => uint256) public shareHolder;
 
     event TransferShares(address indexed from, address indexed to, uint256 value);
 
-    constructor(ERC20 _asset, string memory _name, string memory _symbol) ERC4626(_asset) ERC20(_name, _symbol) {
-        _vUSDT = _asset;
+    // constructor(ERC20 _asset, string memory _name, string memory _symbol) ERC4626(_asset) ERC20(_name, _symbol) {
+    //     _vUSDT = _asset;
+    // }
+    constructor(address _usdtAddress, string memory _name, string memory _symbol)
+        ERC4626(IERC20(_usdtAddress))
+        ERC20(_name, _symbol)
+    {
+        usdt = IERC20(_usdtAddress);
     }
 
     // Info: returns total number of assets (20230615 - tzuhan)
     function totalAssets() public view override returns (uint256) {
-        return _vUSDT.balanceOf(address(this));
+        // return _vUSDT.balanceOf(address(this));
+        return usdt.balanceOf(address(this));
     }
 
     // Info: returns total balance of user (20230615 - tzuhan)
     function totalAssetsOfUser(address _user) public view returns (uint256) {
-        return _vUSDT.balanceOf(_user);
+        // return _vUSDT.balanceOf(_user);
+        return usdt.balanceOf(_user);
     }
 
-    function totalShares() public view returns (uint256) {
-        return this.balanceOf(address(this));
-    }
+    // function totalShares() public view returns (uint256) {
+    //     return this.balanceOf(address(this));
+    // }
 
     function totalSharesOfUser(address _user) public view returns (uint256) {
-        // return this.balanceOf(_user);
         return shareHolder[_user];
     }
 
@@ -40,7 +50,8 @@ contract Vault is ERC4626 {
         // Info: checks that the deposit is higher than 0 (20230615 - tzuhan)
         require(assets > 0, "Deposit less than Zero");
 
-        _vUSDT.transferFrom(msg.sender, address(this), assets);
+        // _vUSDT.transferFrom(msg.sender, address(this), assets);
+        usdt.transferFrom(msg.sender, address(this), assets);
 
         // Info: checks the value of assets the holder has (20230615 - tzuhan)
         shareHolder[msg.sender] += assets;
@@ -72,17 +83,18 @@ contract Vault is ERC4626 {
     // Info:  allow msg.sender to withdraw his deposit plus interest (20230615 - tzuhan)
     function withdraw(uint256 shares) public {
         uint256 payout = _redeem(shares);
-        _vUSDT.transfer(msg.sender, payout);
+        // _vUSDT.transfer(msg.sender, payout);
+        usdt.transfer(msg.sender, payout);
     }
 
-    // function transferShares(uint256 shares, address receiver) public {
-    //     _transfer(msg.sender, receiver, shares);
+    function transferShares(uint256 shares, address receiver) public {
+        _transfer(msg.sender, receiver, shares);
 
-    //     shareHolder[msg.sender] -= shares;
-    //     shareHolder[receiver] += shares;
+        shareHolder[msg.sender] -= shares;
+        shareHolder[receiver] += shares;
 
-    //     emit TransferShares(msg.sender, receiver, shares);
-    // }
+        emit TransferShares(msg.sender, receiver, shares);
+    }
 
     function transfer(address receiver, uint256 shares) public virtual override returns (bool) {
         _transfer(msg.sender, receiver, shares);
@@ -94,4 +106,5 @@ contract Vault is ERC4626 {
 
         return true;
     }
+
 }
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -3,43 +3,43 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
-import "../src/USDT.sol";
+// import "../src/USDT.sol";
 
 contract VaultTest is Test {
-    USDT public usdt;
+    // USDT public usdt;
     Vault public vault;
 
     function setUp() public {
-        usdt = new USDT("TideBit USDT", "tbUSDT");
-        vault = new Vault(usdt, "Vault USDT", "vUSDT");
+        // usdt = new USDT("TideBit USDT", "tbUSDT");
+        vault = new Vault(address(0x6e642065B9976FbDF94aB373a4833A48F040BfF3), "Vault USDT", "vUSDT");
     }
 
-    function testTotalAssets() public {
-        emit log_named_uint("the vault totalAssets", vault.totalAssets());
-        assertEq(vault.totalAssets(), 0);
-    }
+    // function testTotalAssets() public {
+    //     emit log_named_uint("the vault totalAssets", vault.totalAssets());
+    //     assertEq(vault.totalAssets(), 0);
+    // }
 
-    function testTotalAssetsOfUser(address user) public {
-        assertEq(vault.totalAssetsOfUser(user), 0);
-    }
+    // function testTotalAssetsOfUser(address user) public {
+    //     assertEq(vault.totalAssetsOfUser(user), 0);
+    // }
 
-    function testMint() public {
-        usdt.mint(address(this), 100);
-        assertEq(usdt.balanceOf(address(this)), 100);
-    }
+    // function testMint() public {
+    //     usdt.mint(address(this), 100);
+    //     assertEq(usdt.balanceOf(address(this)), 100);
+    // }
 
-    function testDeposit() public {
-        usdt.mint(address(this), 100);
-        usdt.approve(address(vault), 100);
-        vault.deposit(100);
-        assertEq(vault.totalAssets(), 100);
-        assertEq(vault.totalAssetsOfUser(address(this)), 100);
-        emit log_named_uint("the vault totalAssets", vault.totalAssets());
-    }
+    // function testDeposit() public {
+    //     usdt.mint(address(this), 100);
+    //     usdt.approve(address(vault), 100);
+    //     vault.deposit(100);
+    //     assertEq(vault.totalAssets(), 100);
+    //     assertEq(vault.totalAssetsOfUser(address(this)), 100);
+    //     emit log_named_uint("the vault totalAssets", vault.totalAssets());
+    // }
 
     function testWithdraw() public {
-        usdt.mint(address(this), 100);
-        usdt.approve(address(vault), 100);
+        // usdt.mint(address(this), 100);
+        // usdt.approve(address(vault), 100);
         vault.deposit(100);
         assertEq(vault.totalAssets(), 100);
         assertEq(vault.totalSharesOfUser(address(this)), 100);
@@ -50,3 +50,4 @@ contract VaultTest is Test {
         assertEq(vault.totalAssets(), 0);
     }
 }
+
