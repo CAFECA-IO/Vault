b4f9954351045ef7890aaae923faf668c5803553
TzuHanLiang <crocodilestear@gmail.com>
2023-06-19 13:37:31 +0800

    feat: update comment

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
-// import "../src/USDT.sol";
+import "../src/USDT.sol";
 
 contract VaultTest is Test {
-    // USDT public usdt;
+    USDT public usdt;
     Vault public vault;
 
     function setUp() public {
-        // usdt = new USDT("TideBit USDT", "tbUSDT");
+        usdt = new USDT("TideBit USDT", "tbUSDT");
         vault = new Vault(address(0x6e642065B9976FbDF94aB373a4833A48F040BfF3), "Vault USDT", "vUSDT");
     }
 
@@ -23,31 +23,32 @@ contract VaultTest is Test {
         assertEq(vault.totalAssetsOfUser(user), 0);
     }
 
-    // function testMint() public {
-    //     usdt.mint(address(this), 100);
-    //     assertEq(usdt.balanceOf(address(this)), 100);
-    // }
-
-    // function testDeposit() public {
-    //     usdt.mint(address(this), 100);
-    //     usdt.approve(address(vault), 100);
-    //     vault.deposit(100);
-    //     assertEq(vault.totalAssets(), 100);
-    //     assertEq(vault.totalAssetsOfUser(address(this)), 100);
-    //     emit log_named_uint("the vault totalAssets", vault.totalAssets());
-    // }
-
-    // function testWithdraw() public {
-    //     usdt.mint(address(this), 100);
-    //     usdt.approve(address(vault), 100);
-    //     vault.deposit(100);
-    //     assertEq(vault.totalAssets(), 100);
-    //     assertEq(vault.totalSharesOfUser(address(this)), 100);
-    //     assertEq(vault.totalAssetsOfUser(address(this)), 0);
-    //     vault.withdraw(100);
-    //     assertEq(vault.totalSharesOfUser(address(this)), 0);
-    //     assertEq(vault.totalAssetsOfUser(address(this)), 100);
-    //     assertEq(vault.totalAssets(), 0);
-    // }
+    function testMint() public {
+        usdt.mint(address(this), 100);
+        assertEq(usdt.balanceOf(address(this)), 100);
+    }
+    /**
+     * Deprecate: currently vault is not using custom ERC20 token, cannot use this way to test deposit or withdraw (20230619 - tzuhan)
+     * function testDeposit() public {
+     *     usdt.mint(address(this), 100);
+     *     usdt.approve(address(vault), 100);
+     *     vault.deposit(100);
+     *     assertEq(vault.totalAssets(), 100);
+     *     assertEq(vault.totalAssetsOfUser(address(this)), 100);
+     *     emit log_named_uint("the vault totalAssets", vault.totalAssets());
+     * }
+     * 
+     * function testWithdraw() public {
+     *     usdt.mint(address(this), 100);
+     *     usdt.approve(address(vault), 100);
+     *     vault.deposit(100);
+     *     assertEq(vault.totalAssets(), 100);
+     *     assertEq(vault.totalSharesOfUser(address(this)), 100);
+     *     assertEq(vault.totalAssetsOfUser(address(this)), 0);
+     *     vault.withdraw(100);
+     *     assertEq(vault.totalSharesOfUser(address(this)), 0);
+     *     assertEq(vault.totalAssetsOfUser(address(this)), 100);
+     *     assertEq(vault.totalAssets(), 0);
+     * }
+     */
 }
-
